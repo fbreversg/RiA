d5ef86036c83f78de799f6944b2753be417a19da
Paco <paco.brevers@ipronics.com>
2023-01-18 20:21:49 +0100

    Use file as input

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,8 +1,9 @@
-// cargo run -- pattern
-
 extern crate regex;
 extern crate clap;
 
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 use regex::Regex;
 use clap::{App, Arg};
 
@@ -16,17 +17,21 @@ fn main() {
             .help("The pattern to search for")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .required(true))
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop, bedroom window, public-house, and
-   dark square is a picture feverishly turned--in search of what?
-   It is the same with books. What do we seek through millions of pages?";
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        match re.find(line) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => ()
         }
