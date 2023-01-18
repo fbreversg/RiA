f556915e7e3062881df0d5cd43ab46850086b4f7
Paco <paco.brevers@ipronics.com>
2023-01-18 20:30:52 +0100

    Add stdin reading caps

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -2,11 +2,21 @@ extern crate regex;
 extern crate clap;
 
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 use regex::Regex;
 use clap::{App, Arg};
 
+fn process_lines<T: BufRead + Sized>(reader: T, re:Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => ()
+        }
+    } 
+}
 
 fn main() {
 
@@ -25,15 +35,15 @@ fn main() {
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
+    let input = args.value_of("input").unwrap_or("-");
 
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        match re.find(&line) {
-            Some(_) => println!("{}", line),
-            None => ()
-        }
-    } 
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
+    }
 } 
