6ba5faf9ce44c6286280d261d262094a28cdfd95
Paco <paco.brevers@ipronics.com>
2023-01-18 15:56:11 +0100

    Use enumerate

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -2,11 +2,13 @@ fn main() {
    let search_term = "picture";
    let quote = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+It is the same with books. What do we seek through millions of pages?"; 
 
-    for line in quote.lines() {
+    for (idx, line) in quote.lines().enumerate() {
         if line.contains(search_term) {
-            println!("{}", line)
+            let line_num = idx + 1;
+            println!("{}: {}", line_num, line);
         }
+     
     }
 }
