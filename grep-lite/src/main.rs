eb9c58af101d5efdc2dd184fb43eaffecbd7cb36
Paco <paco.brevers@ipronics.com>
2023-01-18 19:07:15 +0100

    Use regex

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,53 +1,20 @@
+extern crate regex;
+
+use regex::Regex;
+
+
+
 fn main() {
-   //Parameters
-   let context_lines = 1;
-   let needle = "oo";
-   let haystack = "Every face, every shop,
-   bedroom window, public-house, and
-   dark square is a picture
-   feverishly turned--in search of what?
-   It is the same with books.
-   What do we seek
-   through millions of pages?";
+    let re = Regex::new("picture").unwrap();
 
-    // Initialization
-    let mut tags: Vec<usize> = Vec::new();
-    let mut ctx: Vec<Vec<(usize, String)>> = Vec::new();
+    let quote = "Every face, every shop, bedroom window, public-house, and
+   dark square is a picture feverishly turned--in search of what?
+   It is the same with books. What do we seek through millions of pages?";
 
-    // Pass 1
-    for (idx, line) in haystack.lines().enumerate() {
-        if line.contains(needle) {
-            tags.push(idx);
-
-            let v = Vec::with_capacity(2*context_lines + 1);
-            ctx.push(v);
+    for line in quote.lines() {
+        match re.find(line) {
+            Some(_) => println!("{}", line),
+            None => ()
         }
-    }
-
-    if tags.len() == 0 {
-        return;
-    }
-    
-    // Pass 2
-    for (idx, line) in haystack.lines().enumerate() {
-        for (jdx, tag) in tags.iter().enumerate() {
-            let lower_bound = tag.saturating_sub(context_lines);
-            let upper_bound = tag + context_lines;
-
-            if (idx >= lower_bound) && (idx<=upper_bound) {
-                let line_as_string = String::from(line);
-                let local_ctx = (idx, line_as_string);
-                ctx[jdx].push(local_ctx);
-            }
-        }
-    }
-
-    // Output
-    for local_ctx in ctx.iter() {
-        for &(idx, ref line) in local_ctx.iter() {
-            let line_num = idx + 1;
-            println!("{}: {}", line_num, line);
-        }
-    }
-
-}
+    } 
+} 
